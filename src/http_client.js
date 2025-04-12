2f2bdfa1930593db737107719214698795277264
tazuya <il.gasnikov2016@yandex.ru>
2025-04-12 13:37:31 +0300

    remove console logs

--- src/http_client.js
+++ src/http_client.js
@@ -34,14 +34,6 @@ class HttpClient {
         }
       });
 
-      console.log("Event Tracking Response:", {
-        status: response.status,
-        statusText: response.statusText,
-        data: response.data,
-        headers: response.headers,
-        requestTime: new Date().toISOString()
-      });
-
       return {
         success: true,
         status: response.status,
